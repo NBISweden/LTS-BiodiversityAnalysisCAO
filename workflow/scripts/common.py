7e19798163aaea87db6cadfa43b3a548d3a56efd
johnne <john.sundh@scilifelab.se>
2022-12-12 23:15:41 +0100

    Change to use results_dir with constraints
    This change is done in an attempt to clean up how output and wildcards work together

--- workflow/scripts/common.py
+++ workflow/scripts/common.py
@@ -2,8 +2,8 @@ def all_output(wildcards):
     output = []
     output.extend(
         expand(
-            "{out_folder}/results/mappings/{map_name}/{mapper}/{sample}_{seqtype}.fastq.gz",
-            out_folder=config["out_folder"],
+            "{results_dir}/mappings/{map_name}/{mapper}/{sample}_{seqtype}.fastq.gz",
+            results_dir=config["results_dir"],
             map_name=mappings.keys(),
             mapper=config["mappers"],
             sample=samples.keys(),
@@ -13,32 +13,31 @@ def all_output(wildcards):
     # extend with fish mapping
     output.extend(
         expand(
-            "{out_folder}/results/genome_mappings/{ref}/counts/{sample}_{t}_counts.txt",
+            "{results_dir}/genome_mappings/{ref}/counts/{sample}_{t}_counts.txt",
+            results_dir=config["results_dir"],
             sample=samples.keys(),
             t=["taxid","species"],
             ref=config["mappings"]["genomes"].keys(),
-            out_folder=config["out_folder"],
         )
     )
     output.extend(
         expand(
-            "{out_folder}/results/multiqc/multiqc.html",
-            out_folder=config["out_folder"],
+            "{results_dir}/multiqc/multiqc.html", results_dir=config["results_dir"]
         )
     )
     output.extend(
         expand(
-            "{out_folder}/results/mappings/{map_name}/map_qc.html",
-            out_folder=config["out_folder"],
+            "{results_dir}/mappings/{map_name}/map_qc.html",
             map_name=mappings.keys(),
+            results_dir=config["results_dir"]
         )
     )
     #TODO: Fail explicitly here if sintax database does not exist
     if os.path.exists(config["sintax"]["db"]):
         output.extend(
             expand(
-                "{out_folder}/results/mappings/{map_name}/{mapper}/{sample}.sintax.{suff}",
-                out_folder=config["out_folder"],
+                "{results_dir}/mappings/{map_name}/{mapper}/{sample}.sintax.{suff}",
+                results_dir=config["results_dir"],
                 map_name=mappings.keys(),
                 mapper=config["mappers"],
                 sample=samples.keys(),
@@ -47,8 +46,8 @@ def all_output(wildcards):
         )
         output.extend(
             expand(
-                "{out_folder}/results/mappings/{map_name}/{mapper}/krona/krona.html",
-                out_folder=config["out_folder"],
+                "{results_dir}//mappings/{map_name}/{mapper}/krona/krona.html",
+                results_dir=config["results_dir"],
                 map_name=mappings.keys(),
                 mapper=config["mappers"],
             )
@@ -59,6 +58,6 @@ def all_output(wildcards):
 def krona_input_string(wildcards):
     input_string = []
     for sample in samples.keys():
-        text = f"{wildcards.out_folder}/results/mappings/{wildcards.map_name}/{wildcards.mapper}/{sample}.sintax.parsed.krona.txt,{sample}"
+        text = f"results/mappings/{wildcards.map_name}/{wildcards.mapper}/{sample}.sintax.parsed.krona.txt,{sample}"
         input_string.append(text)
     return " ".join(input_string)
