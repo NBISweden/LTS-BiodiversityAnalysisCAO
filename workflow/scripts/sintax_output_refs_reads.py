d1bd5189ca51f2cd1a0ba148d7b4415063c263de
johnne <john.sundh@scilifelab.se>
2022-12-19 08:51:18 +0100

    Output both reference and query ids

--- workflow/scripts/sintax_output_refs_reads.py
+++ workflow/scripts/sintax_output_refs_reads.py
@@ -4,23 +4,30 @@ from argparse import ArgumentParser
 import pandas as pd
 
 
+def write_reads(outfile, l):
+    with open(outfile, "w") as fhout:
+        for r in l:
+            fhout.write(f"^{r}\n")
+
 def main(args):
     taxdf = pd.read_csv(args.tax, sep="\t", index_col=0)
     # Get rank names
-    sp = taxdf.loc[taxdf[args.rank].str.startswith("Unclassified")][args.rank].unique()
+    sp_hits = taxdf.loc[taxdf[args.rank].str.startswith("Unclassified")]
+    queries = list(sp_hits.index)
+    sp = sp_hits[args.rank].unique()
     dbinfo = pd.read_csv(args.dbinfo, sep="\t", index_col=0)
     # Get database records
     records = dbinfo.loc[dbinfo[args.rank].isin(sp)].index
-    with open(args.outfile, "w") as fhout:
-        for r in records:
-            fhout.write(f"{r}\n")
+    write_reads(records, args.refs_out)
+    write_reads(queries, args.queries_out)
 
 
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("tax", type=str, help="Sintax parsed results")
     parser.add_argument("dbinfo", type=str, help="COIDB info file")
-    parser.add_argument("outfile", type=str, help="Outfile with reads")
+    parser.add_argument("refs_out", type=str, help="Outfile with reference ids")
+    parser.add_argument("queries_out", type=str, help="Outfile with query ids")
     parser.add_argument(
         "--rank", type=str, default="species", help="Rank at which to filter"
     )
