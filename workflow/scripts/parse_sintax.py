2046da95f4278e72e625e4b11414c351a399a5a3
John <john.sundh@scilifelab.se>
2023-03-07 21:28:11 +0100

    Pass replacement ranks to write_krona

--- workflow/scripts/parse_sintax.py
+++ workflow/scripts/parse_sintax.py
@@ -22,8 +22,10 @@ def compair(lineage1, lineage2, conf1, conf2, ranks):
     most resolved one, or (if the are equally resolved) the one with a
     higher confidence value at the lowest assigned rank
     """
-    l1 = [lineage1[x] for x in ranks if not lineage1[x].startswith("Unclassified.")]
-    l2 = [lineage2[x] for x in ranks if not lineage2[x].startswith("Unclassified.")]
+    l1 = [lineage1[x] for x in ranks if
+          not lineage1[x].startswith("Unclassified.")]
+    l2 = [lineage2[x] for x in ranks if
+          not lineage2[x].startswith("Unclassified.")]
     # Compare number of matching taxlabels
     matching = set(l1).intersection(l2)
     if len(l1) > len(l2):
@@ -46,6 +48,7 @@ def replace_ranks(dataf, replacements):
         d[key] = value
     return dataf.rename(columns=d), new_ranks
 
+
 def write_krona(dataf, ranks, replacement_ranks, outfile):
     if replacement_ranks:
         r = {}
@@ -58,7 +61,8 @@ def write_krona(dataf, ranks, replacement_ranks, outfile):
         with open(outfile, "w") as fhout:
             fhout.write("")
     else:
-        krona_df = pd.DataFrame(dataf.loc[:, ranks].groupby(ranks).size()).reset_index()
+        krona_df = pd.DataFrame(
+            dataf.loc[:, ranks].groupby(ranks).size()).reset_index()
         krona_df.loc[:, [0] + ranks].to_csv(
             outfile, index=False, sep="\t", header=False
         )
@@ -76,7 +80,8 @@ def main(args):
         "s": "species",
     }
     if not args.ranks:
-        ranks = ["domain", "kingdom", "phylum", "class", "order", "family", "genus", "species"]
+        ranks = ["domain", "kingdom", "phylum", "class", "order",
+                 "family", "genus", "species"]
     else:
         ranks = args.ranks
     taxdict = {}
@@ -87,7 +92,8 @@ def main(args):
         for line in fhin:
             # For some reason sintax sometimes writes lines with only the seqid
             try:
-                seqid, taxres, strand, taxlineage = line.rstrip().split("\t")
+                seqid, taxres, strand, taxlineage = line.rstrip().split(
+                    "\t")
             except ValueError:
                 continue
             lineage = {}
@@ -106,7 +112,8 @@ def main(args):
             if seqid in taxdict.keys():
                 lineage1 = taxdict[seqid]
                 conf1 = confdict[seqid]
-                lineage, conf, matching = compair(lineage1, lineage, conf1, conf, ranks)
+                lineage, conf, matching = compair(lineage1, lineage,
+                                                  conf1, conf, ranks)
                 lineage["matching"] = matching
             confdict[seqid] = float(conf)
             taxdict[seqid] = lineage
@@ -116,21 +123,26 @@ def main(args):
     if args.krona:
         if len(new_ranks) > 0:
             ranks = new_ranks
-        write_krona(dataf, ranks, args.krona)
+        write_krona(dataf, ranks, new_ranks, args.krona)
 
 
 if __name__ == "__main__":
     parser = ArgumentParser()
-    parser.add_argument("sintax_results", type=str, help="Sintax output file")
+    parser.add_argument("sintax_results", type=str,
+                        help="Sintax output file")
     parser.add_argument(
-        "-c", "--cutoff", type=float, help="Cutoff threshold (0.8)", default=0.8
+        "-c", "--cutoff", type=float, help="Cutoff threshold (0.8)",
+        default=0.8
     )
     parser.add_argument(
-        "-k", "--krona", type=str, help="Write krona compatible output to file"
+        "-k", "--krona", type=str,
+        help="Write krona compatible output to file"
     )
     parser.add_argument(
-        "-r", "--ranks", nargs="*", help="Ranks used in the SINTAX database"
+        "-r", "--ranks", nargs="*",
+        help="Ranks used in the SINTAX database"
     )
-    parser.add_argument("--replace_ranks", nargs="*", help="Replace rank names in final output (<rank>=<newrank>)")
+    parser.add_argument("--replace_ranks", nargs="*",
+                        help="Replace rank names in final output (<rank>=<newrank>)")
     args = parser.parse_args()
     main(args)
